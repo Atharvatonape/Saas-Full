15b94d3c3e74343e2c9dc9792635e48bb58ef577
Atharvatonape <athravtonape1072@gmail.com>
2024-06-17 17:14:39 +0200

    Ready for Production

--- src/saas/views.py
+++ src/saas/views.py
@@ -2,7 +2,10 @@ from django.http  import HttpResponse
 from django.shortcuts import render
 from visits.models import PageVisit
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path = request.path)
     my_title = "Main page"
