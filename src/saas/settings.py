0ba64e84129ef9d76a7286bddd6887cd0d4f694b
Atharvatonape <athravtonape1072@gmail.com>
2024-06-19 16:52:30 +0200

    Secret Key

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wma)o9unt+=ozx&6v*#@8*)k89fyr@fi+e8dqm_&cqep5j*1^5'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = config("DJANGO_DEBUG", cast = bool)
+print(DEBUG)
 ALLOWED_HOSTS = [
     ".railway.app"
 ]
