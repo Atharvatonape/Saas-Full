65c9c6b3ac196639a1ab1a30259b8925ce28d48c
Atharva Tonape <atharva.tonape@t-systems.com>
2024-07-29 13:10:10 +0200

    4 - Test Django Actions Secret Database URL

--- src/commando/tests.py
+++ src/commando/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.conf import settings
 
 # Create your tests here.
+class NeonDBTestCase(TestCase):
+
+    def test_db_url(self):
+        DATABASE_URL = settings.DATABASE_URL
+        self.assertIn("neon.tech", DATABASE_URL)
